940ed7be342c2461eabf2bbb4516b87efd2b163a
RebellWrynn <egzinivan@gmail.com>
2025-04-14 14:53:33 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+import time
+
+# def _first_ramdom_num():
+time_ = time.time()
+x = int(time_)
+x1 = int(x) // 1 
+x2 = int(x) / 5 * 10
+x3 = int(x) / 9
+num = x1+x2+x3 
+
+print (x)
+
+
+# def second_random_num(param2: int) ->int:
+
+# def test_sum()
