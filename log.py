c918d66913daa6c81f093cd2f7e4be8d4f4b0e3f
RebellWrunn <egzinivan@gmail.com>
2025-06-06 14:33:26 +0500

    2

--- log.py
+++ log.py
@@ -1,8 +1,11 @@
 num = float(input())
-e = 2.7
+e = 2.71828182845904
 _cout = 1 
-while e < num :
-    e = e*e
+while e <= num :
+    e = e*2.71828182845904
     _cout +=1 
+
+if e > num:
+
 
 print(_cout)
